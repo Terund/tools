38220d25dfa7f35a61127d8d5a2c89b782257796
moming <1490932430@qq.com>
2019-10-29 16:11:07 +0800

    增加redis数据库操作

---
+++ 代码/redis.py
@@ -0,0 +1,10 @@
+﻿import redis
+
+REDIS_HOST = "127.0.0.1"
+REDIS_PORT = 6379
+
+rdb = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+# 往redis数据库插入数据
+rdb.set("key", "value")
+# 从redis数据库获取数据
+print(rdb.get("key"))
