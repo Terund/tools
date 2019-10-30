b6b585ea993a1c6b129de141ac5c965fcfe2723e
moming <1490932430@qq.com>
2019-10-30 13:38:10 +0800

    修改了邮件发送文件

--- 代码/operate_email.py
+++ 代码/operate_email.py
@@ -3,41 +3,41 @@ import smtplib
 import poplib
 import imaplib
 from email.mime.text import MIMEText
-from email.header import Header
+
+
+# from email.header import Header
+
+
 class OperateEmail:
     # 此函数通过使用smtplib实现发送邮件
-    def send_email_by_smtp(self):
+    def send_email_by_smtp(self, *args, security_code=None):
         # 用于发送邮件的邮箱。修改成自己的邮箱
-        sender_email_address = "1490932430@qq.com"
+        sender_email_address = "iamhwmin@163.com"
         # 用于发送邮件的邮箱的密码。修改成自己的邮箱的密码
-        sender_email_password = "abkhgephglbzfibc"
-        # 用于发送邮件的邮箱的smtp服务器，也可以直接是IP地址
-        # 修改成自己邮箱的sntp服务器地址；qq邮箱不需要修改此值
-        smtp_server_host = "smtp.qq.com"
-        # 修改成自己邮箱的sntp服务器监听的端口；qq邮箱不需要修改此值
-        smtp_server_port = 465
-        # 要发往的邮箱
-        receiver_email = "your_dest_email@qq.com"
+        sender_email_password = "iamheweimin007"
+        # 邮件要发往的地址
+        receiver_email = ",".join(list(args))
         # 要发送的邮件主题
-        message_subject = "Python smtp测试邮件"
+        message_subject = "何伟民"
         # 要发送的邮件内容
-        message_context = "这是一封通过Python smtp发送的测试邮件..."
-
+        message_context = """
+            这是一封邮件{}
+        """.format(security_code)
         # 邮件对象，用于构建邮件
         message = MIMEText(message_context, 'plain', 'utf-8')
         # 设置发件人（声称的）
-        message["From"] = Header(sender_email_address, "utf-8")
+        message["From"] = sender_email_address
         # 设置收件人（声称的）
-        message["To"] = Header(receiver_email, "utf-8")
+        message["To"] = receiver_email
         # 设置邮件主题
-        message["Subject"] = Header(message_subject,"utf-8")
+        message["Subject"] = message_subject
 
         # 连接smtp服务器。如果没有使用SSL，将SMTP_SSL()改成SMTP()即可其他都不需要做改动
-        email_client = smtplib.SMTP_SSL(smtp_server_host, smtp_server_port)
+        email_client = smtplib.SMTP("smtp.163.com", 25)
         try:
             # 验证邮箱及密码是否正确
             email_client.login(sender_email_address, sender_email_password)
-            print("smtp----login success, now will send an email to {receiver_email}")
+            print("smtp----login success, now will send an email to {}".format(receiver_email))
         except:
             print("smtp----sorry, username or password not correct or another problem occur")
         else:
@@ -51,9 +51,9 @@ class OperateEmail:
     # 此函数通过使用poplib实现接收邮件
     def recv_email_by_pop3(self):
         # 要进行邮件接收的邮箱。改成自己的邮箱
-        email_address = "your_email@qq.com"
+        email_address = "1490932430@qq.com"
         # 要进行邮件接收的邮箱的密码。改成自己的邮箱的密码
-        email_password = "your_email_password"
+        email_password = "abkhgephglbzfibc"
         # 邮箱对应的pop服务器，也可以直接是IP地址
         # 改成自己邮箱的pop服务器；qq邮箱不需要修改此值
         pop_server_host = "pop.qq.com"
@@ -88,7 +88,7 @@ class OperateEmail:
         # 通过retr(index)读取第index封邮件的内容；这里读取最后一封，也即最新收到的那一封邮件
         resp, lines, octets = email_server.retr(email_count)
         # lines是邮件内容，列表形式使用join拼成一个byte变量
-        email_content = b'\r\n'.join(lines)
+        email_content = b"\r\n".join(lines)
         # 再将邮件内容由byte转成str类型
         email_content = email_content.decode()
         print(email_content)
@@ -99,9 +99,9 @@ class OperateEmail:
     # 此函数通过使用imaplib实现接收邮件
     def recv_email_by_imap4(self):
         # 要进行邮件接收的邮箱。改成自己的邮箱
-        email_address = "your_email@qq.com"
+        email_address = "1490932430@qq.com"
         # 要进行邮件接收的邮箱的密码。改成自己的邮箱的密码
-        email_password = "your_email_password"
+        email_password = "abkhgephglbzfibc"
         # 邮箱对应的imap服务器，也可以直接是IP地址
         # 改成自己邮箱的imap服务器；qq邮箱不需要修改此值
         imap_server_host = "imap.qq.com"
@@ -117,7 +117,7 @@ class OperateEmail:
             exit(1)
         try:
             # 验证邮箱及密码是否正确
-            email_server.login(email_address,email_password)
+            email_server.login(email_address, email_password)
             print("imap4----username exist, now will check password")
         except:
             print("imap4----sorry the given email address or password seem do not correct")
@@ -136,12 +136,7 @@ class OperateEmail:
         # 关闭连接
         email_server.logout()
 
-if __name__ == "__main__":
-    # 实例化
-    email_client = operate_email()
-    # 调用通过smtp发送邮件的发送函数
-    email_client.send_email_by_smtp()
-    # 调用通过pop3接收邮件的接收函数
-    email_client.recv_email_by_pop3()
-    # 调用通过imap4接收邮件的接收函数
-    email_client.recv_email_by_imap4()
+
+if __name__ == '__main__':
+    email = OperateEmail()
+    email.send_email_by_smtp("iamhwm@qq.com", security_code=233333)
