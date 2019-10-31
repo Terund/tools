478d432666debf0830641ec79674c8c6d215d8e3
moming <1490932430@qq.com>
2019-10-31 19:50:58 +0800

    增加下载网页图片功能

---
+++ 代码/save_image.py
@@ -0,0 +1,14 @@
+﻿import requests
+from PIL import Image
+
+
+# 保存图片到本地
+def get_image(url):
+    """
+    :param url: 图片链接参数
+    :return: 无返回值，直接保存一张图片
+    """
+    response = requests.get(url)
+    with open("price.png", "wb") as fp:
+        for data in response.iter_content(128):
+            fp.write(data)
